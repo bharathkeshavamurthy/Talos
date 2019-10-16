b84c704eb6e6783c0c231c4a6aa347a791d47e21
Bharath Keshavamurthy <bkeshava@purdue.edu>
2019-10-16 03:36:08 -0400

    Don't commit the build directory

--- src/main/cpp/org/purdue/ece/talos/optimize/.dep/crt1.o.d
+++
@@ -1,5 +0,0 @@
-build/obj/crt1.o: \
- ../e-puck2_main-processor/ChibiOS//os/common/ports/ARMCMx/compilers/GCC/crt1.c \
- /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stdbool.h
-
-/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stdbool.h:
