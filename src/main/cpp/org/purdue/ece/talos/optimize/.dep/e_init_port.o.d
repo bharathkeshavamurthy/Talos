898394fc5fad9539fb78e5b09a934440de12c45d
Bharath Keshavamurthy <bkeshava@purdue.edu>
2019-11-01 05:18:31 -0400

    Cleaning build directories and removing unnecessary firmwares

--- src/main/cpp/org/purdue/ece/talos/optimize/.dep/e_init_port.o.d
+++
@@ -1,5 +0,0 @@
-build/obj/e_init_port.o: \
- ../e-puck2_main-processor/src/epuck1x/motor_led/e_init_port.c \
- ../e-puck2_main-processor/src/epuck1x/motor_led/e_init_port.h
-
-../e-puck2_main-processor/src/epuck1x/motor_led/e_init_port.h:
