e0984f244c169682444812f8da4ad2238f8cf5ae
Bharath Keshavamurthy <bkeshava@purdue.edu>
2019-11-12 00:01:04 -0500

    Dist. Active Mesh: Optimize- Removing the build directories

--- src/main/cpp/org/purdue/ece/talos/optimize/.dep/DataEEPROM.o.d
+++
@@ -1,5 +0,0 @@
-build/obj/DataEEPROM.o: \
- ../e-puck2_main-processor/src/epuck1x/DataEEPROM.c \
- ../e-puck2_main-processor/src/epuck1x/DataEEPROM.h
-
-../e-puck2_main-processor/src/epuck1x/DataEEPROM.h:
