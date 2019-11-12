a269317f31d9fb451e8c8405c39f4cc2fc5514c2
Bharath Keshavamurthy <bkeshava@purdue.edu>
2019-11-12 16:04:52 -0500

    Gateway: Unused events are removed

--- src/main/cpp/org/purdue/ece/talos/gateway/main/main.c
+++ src/main/cpp/org/purdue/ece/talos/gateway/main/main.c
@@ -46,12 +46,12 @@ const int WIFI_CONNECTED_BIT = BIT0;
 static void access_point_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
 	/* Connection Event */
 	if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-		wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-		/* Do some stuff here */
+		/* wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data; */
+		/* Do some stuff here using the WIFI_EVENT_AP_STACONNECTED event */
 	/* Disconnection Event */
 	} else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-		wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-		/* Do some stuff here */
+		/* wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data; */
+		/* Do some stuff here with the WIFI_EVENT_AP_STADISCONNECTED event */
 	}
 }
 
